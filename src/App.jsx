865df26e7d181dcd7f1b8c7f43bf13d781abe5e1
PriaAdmaja <priaadmaja@gmail.com>
2023-09-22 14:50:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Home from "./components/home";
+
+function App() {
+  return (
+    <>
+      <main className="w-[450px] mx-auto">
+        <Home />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="h-screen bg-home bg-cover">
+      <div className=" ">
+        <p>ini home</p>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
