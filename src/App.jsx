eae71dbecfbba69b7ad5bfe061f84df3151dbf4f
PriaAdmaja <priaadmaja@gmail.com>
2023-10-09 12:30:24 +0700

    fix audio

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,26 @@ import Acara from "./components/acara";
 import ArRum from "./components/arRum";
 import Detail from "./components/detail";
 import Home from "./components/home";
-import Map from "./components/map";
 import Notes from "./components/notes";
-// import playBtn from "./assets/play-button.png";
-// import pauseBtn from "./assets/pause.png";
+import playBtn from "./assets/play-button.png";
+import pauseBtn from "./assets/pause-button.png";
+import music from "/music.mp3"
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isPlay, setIsPlay] = useState(false);
+  const [audio, setAudio] = useState(new Audio(music));
 
-  const musicSrc = "/music.mp3";
-  // const musicSrc = "https://soundcloud.com/corbel-keep/christina-perri-a-thousand-10?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing";
+
+
   const playMusic = () => {
-    const audio = new Audio(musicSrc);
-    audio.play();
-    // console.log("music");
+    if(isPlay === false) {
+      setIsPlay(true);
+      audio.play();
+      return
+    }
+    setIsPlay(false);
+    audio.pause()
   };
   return (
     <>
@@ -30,13 +36,20 @@ function App() {
           <Detail />
           <Acara />
           <Notes />
-          <button
-            type="button"
-            className="w-10 h-10 rounded-full fixed bottom-1/4 left-1"
-          >
-            {/* <img src={pause} alt="pause" />
-            <img src={play} alt="play" /> */}
-          </button>
+          <div className="rounded-full fixed bottom-1/4 left-1">
+            <button type="button" className={`w-10 h-10 `} onClick={playMusic}>
+              <img
+                src={playBtn}
+                alt="play"
+                className={`${isPlay ? "hidden" : "block"}`}
+              />
+              <img
+                src={pauseBtn}
+                alt="pause"
+                className={`${isPlay ? "block" : "hidden"}`}
+              />
+            </button>
+          </div>
         </section>
       </main>
     </>
